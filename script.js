db6a3e1b329cef108d912a590921b22689279e9c
Rafey <rafeldo36@gmail.com>
2022-12-15 12:30:38 +0530

    first commit

--- script.js
+++ script.js
@@ -6,6 +6,28 @@ const options = {
 	}
 };
 
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML =  h + ":" + m + ":" + s;
+    setTimeout(startTime, 1000);
+  }
+  
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
+
+  date = new Date();
+  year = date.getFullYear();
+  month = date.getMonth() + 1;
+  day = date.getDate();
+  document.getElementById("current_date").innerHTML = day + "/" + month + "/" + year;
+
 const getTime = (city)=>{
 
 fetch('https://muslimsalat.p.rapidapi.com/'+city, options)
